c6fd703cff86837e101d25097cc74bbf79a0ed36
Penjuyu <s.pochemuchka@mail.ru>
2018-06-06 10:40:53 +0300

    test

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{   
+    public float speed = 0.001f;
+    public float distance = 8;
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log(speed);
+        transform.Translate(3, 0, 0); //скорость смещения объекта
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >= 5 || transform.position.x <= -5)
+        {
+            speed = -speed;
+            
+        }
+        transform.Translate(speed, 0, 0);
+
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speed = 10;
+    public float rotSpeed = 100;
+    public float jumpSpeed = 30; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        float movX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float movZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float rotY = Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        float movY = 0;
+        if (Input.GetButtonDown("Jump"))
+        {
+            movY = jumpSpeed * Time.deltaTime;
+        }
+        transform.Translate(movX, movY, movZ);
+        transform.Rotate(0, rotY, 0);
+    }    
+
+    void OnCollisionEnter(Collision collision)
+    {        
+      if(collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Game Over");
+            GetComponent<Renderer>().material.color = Color.red; //получаем компонент
+            Time.timeScale = 0;// останавливает время в игре
+        }
+
+
+    }
+}
