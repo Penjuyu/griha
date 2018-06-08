56a5b1b4f400a1bb67d83fbc4e310a0b41e7efe8
Penjuyu <s.pochemuchka@mail.ru>
2018-06-08 14:16:38 +0300

    menu

---
+++ Assets/Scripts/VRController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))] // зависимость от объекта
+public class VRController : MonoBehaviour
+{
+    public Transform cam;
+    public float speed = 3;
+    public float toggleAngle = 45;
+    Rigidbody body; // вводим переменную (ссылка на объект)
+    bool isMoving;
+	void Start ()
+    {
+        body = GetComponent<Rigidbody>(); // если у объетка есть rigitbody, то он находит его
+	}
+	
+	
+	void FixedUpdate ()
+    {
+        isMoving = cam.eulerAngles.x >= toggleAngle && cam.eulerAngles.x <= 90;
+       if (isMoving)
+        
+      
+        {
+
+            Vector3 forward = cam.TransformDirection(Vector3.forward);
+            body.velocity = forward * speed;
+        }
+
+	}
+}
